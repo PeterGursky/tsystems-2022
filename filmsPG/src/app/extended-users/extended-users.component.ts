34a64af370eec08e2b33901e9f138f029c69c2fd
Peter Gurský <peter.gursky@upjs.sk>
2022-07-19 14:26:33 +0200

    filter

--- filmsPG/src/app/extended-users/extended-users.component.ts
+++ filmsPG/src/app/extended-users/extended-users.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/entities/user';
 import { UsersService } from 'src/services/users.service';
@@ -14,6 +15,7 @@ export class ExtendedUsersComponent implements OnInit, AfterViewInit {
   users: User[] = [];
   usersDataSource = new MatTableDataSource<User>();
   @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+  @ViewChild(MatSort) sort: MatSort | null = null;
 
   constructor(private usersService: UsersService) { }
 
@@ -27,5 +29,18 @@ export class ExtendedUsersComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.usersDataSource.paginator = this.paginator;
+    this.usersDataSource.sort = this.sort;
+    this.usersDataSource.filterPredicate = (user: User, filter: string):boolean => {
+      if (user.name.toLowerCase().includes(filter)) return true;
+      if (user.email.toLowerCase().includes(filter)) return true;
+      if (user.lastLogin?.toString().toLowerCase().includes(filter)) return true;
+      if (user.groups.map(g => g.name).some(name => name.includes(filter))) return true;
+      return false;
+    };
+  }
+
+  applyFilter(event: any) {
+    const filterStr = event.target.value.trim().toLowerCase();
+    this.usersDataSource.filter = filterStr;
   }
 }
