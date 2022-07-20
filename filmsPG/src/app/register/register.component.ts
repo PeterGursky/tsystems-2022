03d112dbae5a897cac53bfbfbc4746425bfa72fb
Peter Gurský <peter.gursky@upjs.sk>
2022-07-20 13:50:39 +0200

    passwordsMatchValidator

--- filmsPG/src/app/register/register.component.ts
+++ filmsPG/src/app/register/register.component.ts
@@ -9,6 +9,14 @@ import * as zxcvbn from 'zxcvbn';
 })
 export class RegisterComponent implements OnInit {
   hide = true;
+
+  passwordValidator = (control: AbstractControl): ValidationErrors | null => {
+    const result = zxcvbn(control.value);
+    const message = "Password strength: " + result.score + " of 4 - must be 3 or 4. Guessable in " + result.crack_times_display.offline_slow_hashing_1e4_per_second;
+    this.passwordMessage = message;
+    return result.score < 3 ? { weakPassword: message} : null;
+  }
+
   registerForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, 
@@ -16,18 +24,25 @@ export class RegisterComponent implements OnInit {
                                 Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")]),
     password: new FormControl('', this.passwordValidator),
     password2: new FormControl('')
-  });
+  }, this.passwordsMatchValidator);
+  passwordMessage = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  passwordValidator(control: AbstractControl): ValidationErrors | null {
-    const result = zxcvbn(control.value);
-    const message = "Password strength: " + result.score + " of 4 - must be 3 or 4. Guessable in " + result.crack_times_display.offline_slow_hashing_1e4_per_second;
-
-    return result.score < 3 ? { weakPassword: message} : null;
+  passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password');
+    const password2 = control.get('password2');
+    if (password?.value === password2?.value) {
+      password2?.setErrors(null);
+      return null;
+    } else {
+      const err = {"differentPasswords" : "Passwords do not match"}
+      password2?.setErrors(err);
+      return err;
+    }
   }
 
   onSubmit(){
