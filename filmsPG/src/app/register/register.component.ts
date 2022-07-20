d957fee247b6d602ff9ad8882355ab716cab0886
Peter Gurský <peter.gursky@upjs.sk>
2022-07-20 11:58:45 +0200

    password validator

--- filmsPG/src/app/register/register.component.ts
+++ filmsPG/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import * as zxcvbn from 'zxcvbn';
 
 @Component({
   selector: 'app-register',
@@ -9,9 +10,11 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   hide = true;
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, 
+                                Validators.email, 
+                                Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")]),
+    password: new FormControl('', this.passwordValidator),
     password2: new FormControl('')
   });
 
@@ -20,9 +23,20 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const result = zxcvbn(control.value);
+    const message = "Password strength: " + result.score + " of 4 - must be 3 or 4. Guessable in " + result.crack_times_display.offline_slow_hashing_1e4_per_second;
+
+    return result.score < 3 ? { weakPassword: message} : null;
+  }
+
   onSubmit(){
   }
-  
+
+  getJson(error: any): string {
+    return JSON.stringify(error);
+  }
+
   get name(): FormControl<string|null> {
     return this.registerForm.get('name') as FormControl;
   }
