cb76dd749dc2f9d180a32f1d940e1e39a4686b46
Peter Gurský <peter.gursky@upjs.sk>
2022-07-22 15:58:18 +0200

    chat - finished

--- filmsPG/src/app/chat/chat.component.ts
+++ filmsPG/src/app/chat/chat.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ChatService } from 'src/services/chat.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChatMessage, ChatService } from 'src/services/chat.service';
 import { SnackbarService } from 'src/services/snackbar.service';
 
 @Component({
@@ -7,12 +7,18 @@ import { SnackbarService } from 'src/services/snackbar.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
   name = '';
+  messages: ChatMessage[] = [];
+  messageToSend = '';
 
   constructor(private chatService: ChatService, 
               private snackbarService: SnackbarService) { }
+
+  ngOnDestroy(): void {
+    this.chatService.disconnect();
+  }
 
   ngOnInit(): void {
   }
@@ -21,10 +27,24 @@ export class ChatComponent implements OnInit {
     this.chatService.connect().subscribe(success => {
       if (success) {
         this.snackbarService.successMessage("Connected to chat server");
+        this.chatService.listenGreetings().subscribe(greeting => {
+          this.messages = [...this.messages, new ChatMessage("SERVER", greeting)];
+        });
+        this.chatService.listenMessages().subscribe(message => {
+          this.messages = [...this.messages, message];
+        });
         this.chatService.sendHello(this.name);
       } else {
         this.snackbarService.errorMessage("Cannot connect to chat server.");
       }
     });
+  }
+
+  sendMessage(){
+    this.chatService.sendMessage(this.messageToSend);
+  }
+
+  disconnect(){
+    this.chatService.disconnect();
   }
 }
--- filmsPG/src/services/chat.service.ts
+++ filmsPG/src/services/chat.service.ts
@@ -9,6 +9,7 @@ export class ChatService {
   webSocket? : WebSocket;
   stompClient?: Stomp.Client;
   wsUrl = "ws://localhost:8080/ws";
+  myName = '';
 
   constructor() { }
 
@@ -22,6 +23,40 @@ export class ChatService {
 
   sendHello(name: string) {
     this.stompClient?.send("/app/hello", {}, '{"name":"' + name + '"}');
+    this.myName = name;
 //    this.stompClient?.send("/app/hello", {}, JSON.stringify({name}));
   }
+
+  sendMessage(msg:string) {
+    const message = new ChatMessage(this.myName, msg);
+    this.stompClient?.send("/app/message",{},JSON.stringify(message));
+  }
+
+  listenGreetings():Observable<string> {
+    return new Observable( subscriber => {
+      this.stompClient?.subscribe("/topic/greetings", message => {
+        subscriber.next(JSON.parse(message.body).content);
+      });
+    });
+  }
+
+  listenMessages():Observable<ChatMessage> {
+    return new Observable( subscriber => {
+      this.stompClient?.subscribe("/topic/messages", message => {
+        subscriber.next(JSON.parse(message.body));
+      });
+    });
+  }
+
+  disconnect() {
+    this.stompClient?.disconnect(()=>{});
+//    this.webSocket?.close();
+  }
+}
+
+export class ChatMessage {
+  constructor(
+    public name: string,
+    public message: string
+  ){}
 }
